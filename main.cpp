b2d00062d6b7b9794e002dc14f53e1455a2b9f49
hvnshwt <misha.ilin.28school@gmail.com>
2024-10-08 00:09:33 +0300

    postgresql added

--- main.cpp
+++ main.cpp
@@ -1,15 +1,20 @@
 #include <stdio.h>
 #include <curl/curl.h>
- 
+#include <iostream>
+#include <libpq-fe.h>
+
 int main(void)
 {
+  std::cout << "Blya" << std::endl;
+  int libpq_ver = PQlibVersion();
+  printf("Version of libpq: %d\n", libpq_ver);
+
   CURL *curl;
   CURLcode res;
  
   curl = curl_easy_init();
   if(curl) {
-    curl_easy_setopt(curl, CURLOPT_URL, "https://example.com");
-    /* example.com is redirected, so we tell libcurl to follow redirection */
+    curl_easy_setopt(curl, CURLOPT_URL, "https://price.jup.ag/v6/price?ids=DhFkxvDAGCQQdfMXDw8gHfD3KKQBDPvta4tiqEXnvkat");
     curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
  
     /* Perform the request, res gets the return code */
